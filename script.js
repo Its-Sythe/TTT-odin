9f9d1eec00f4a2fd193dff4f09c0ea9faa12a815
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-04-11 08:41:28 +0300

    Added the board and the function that validates a user move

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("git@github.com:Its-Sythe/ttt-odin.git")
+const gameBoard = (function() {
+    let rows = 3;
+    let columns = 3;
+    let board = [];
+
+    for (let a = 0; a < rows; a++) {
+        board.push([]);
+        for (let b = 0; b < columns; b++) {
+            board[a].push(0);
+        }
+    }
+
+    const getBoard = () => board;
+
+    const makeMove = function(row, col, player) {
+        let cell = board[row][col];
+
+        if (cell == 0) {
+            board[row][col] = player;
+            console.log(board);
+        }
+        else if (cell != 0) {
+            return;
+        }
+    } 
+
+    return {
+        getBoard,
+        makeMove
+    }
+})(); 
