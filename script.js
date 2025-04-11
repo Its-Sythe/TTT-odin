3671e5f8daa38a4a3728bb389297c7df9ce3a8e7
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-04-11 09:41:37 +0300

    A currently functional, but full of flaw table validation added

--- script.js
+++ script.js
@@ -17,15 +17,45 @@ const gameBoard = (function() {
 
         if (cell == 0) {
             board[row][col] = player;
-            console.log(board);
         }
         else if (cell != 0) {
             return;
         }
-    } 
+    }
+
+    // board[1][0] = "X"; board[1][1] = "X"; board[1][2] = "X";
+
+    board[0][2] = "O"; board[1][2] = "O"; board[2][2] = "O"
+    board[0][1] = "O"; board[1][1] = "O"; board[2][1] = "O"
+
+    const validateTable = function() {
+        //Row validation
+        if (board[0][0] == board[0][1] && board[0][0] == board[0][2] && board[0][0] != 0) {
+            return `Match won at row 1 by ${board[0][0]}`;
+        } else if (board[1][0] == board[1][1] && board[1][0] == board[1][2] && board[1][0] != 0) {
+            return `Match won at row 2 by ${board[1][0]}`;
+        } else if (board[2][0] == board[2][1] && board[2][0] == board[2][2] && board[2][0] != 0) {
+            return `Match won at row 3 by ${board[2[0]]}`;
+        }
+        //Column validation
+        if (board[0][0] == board[1][0] && board[0][0] == board[2][0] && board[0][0] != 0) {
+            return `Match won at column 1 by ${board[0][0]}`;
+        } else if (board[0][1] == board[1][1] && board[0][1] == board[2][1] && board[0][1] != 0) {
+            return `Match won at column 2 by ${board[0][1]}`;
+        } else if (board[0][2] == board[1][2] && board[0][2] == board[2][2] && board[0][2] != 0) {
+            return `Match won at column 3 by ${board[0][2]}`;
+        }
+        //Diagonal validation
+        if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] != 0) {
+            return `Match won at daigonal by ${board[0][0]}`;
+        } else if (board[0][2] == board[1][1] && board[0][2] == board[2][0] && board[0][2] != 0) {
+            return `Match won at diagonal by ${board[0][2]}`;
+        }
+    }
 
     return {
         getBoard,
-        makeMove
+        makeMove,
+        validateTable
     }
 })(); 
